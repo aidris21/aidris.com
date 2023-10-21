ff8674f7ac410a765b4df648cade0f9c44cf867f
aidris21 <39744048+aidris21@users.noreply.github.com>
2023-10-21 19:22:38 -0400

    add sidebar, fonts, fix type (#5)
    Co-authored-by: Amir Idris <amiridris@amirs-mbp.mynetworksettings.com>

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './Sidebar.css';
+
+interface SidebarProps {
+  pageWrapId: string
+  outerContainerId: string
+}
+
+const Sidebar = (props: SidebarProps): JSX.Element => {
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href="/work">
+        Work
+      </a>
+      <a className="menu-item" href="/music">
+        Music
+      </a>
+      <a className="menu-item" href="/contact">
+        Contact
+      </a>
+    </Menu>
+  );
+};
+
+export default Sidebar

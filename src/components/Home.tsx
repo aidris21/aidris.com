8a7dd51392845b5f5ce237c90bee5da38a501675
Amir Idris <aai2125@columbia.edu>
2025-04-21 21:55:35 -0400

    fix home page styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import background_img from '../images/amiridris.jpeg'
 import foreground from '../images/skyline.svg'
+import SocialLinks from './SocialLinks'
 
 export const Home: React.FC = () => {
     return ( 
-        <div className="h-screen overflow-y-auto overflow-x-hidden perspective-10">
-            <header className="relative flex justify-center items-center h-full preserve-3d -z-10">
-                <img src={foreground} className="absolute h-full w-full object-cover -z-10 transform translate-z-neg-5 scale-150"></img>
-                <h1 className="text-7xl text-white shadow-text">Let's do this one last time...</h1>
-            </header>
-            <section className="text-2xl p-8 bg-gray-800 text-white">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)
-            </section>
+        <div className="min-h-screen w-full bg-black overflow-hidden">
+            <div className="min-h-screen w-full overflow-y-auto overflow-x-hidden perspective-10 bg-gradient-to-b from-[#000B1A] via-[#001233] to-black">
+                <header className="relative flex flex-col justify-center items-center h-screen preserve-3d -z-10">
+                    <div className="mb-8 z-10">
+                        <img 
+                            src={background_img} 
+                            alt="Profile" 
+                            className="w-48 h-48 rounded-full object-cover border-4 border-white shadow-lg"
+                        />
+                    </div>
+                    <img src={foreground} className="absolute inset-0 h-full w-full object-cover -z-10 transform scale-150 opacity-90"></img>
+                    <h1 className="text-7xl text-white shadow-text z-10">Let's do this one last time...</h1>
+                </header>
+                <SocialLinks />
+            </div>
         </div>
     )
 }

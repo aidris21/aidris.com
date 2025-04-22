be600555f96a8b2eb43dacbfde6afb53f29f2d0a
aidris21 <39744048+aidris21@users.noreply.github.com>
2025-04-21 22:03:02 -0400

    Refresh homepage (#7)
    * add tailwind
    
    * fix tailwind issue
    
    * fix home page styling
    
    * change name text
    
    * move home into pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Sidebar from './components/Sidebar';
-import Home from './components/Home'
+import {Home} from './pages/Home'
 
 function App() {
   return (

44e7bd49851dbffc29cb682974235e5437c28bb5
Amir Idris <aai2125@columbia.edu>
2025-05-26 22:07:36 -0400

    finish experience tab

--- src/components/work/ExperienceTab.tsx
+++ src/components/work/ExperienceTab.tsx
@@ -6,6 +6,7 @@ import palantir_technologies_logo from '../../assets/logos/palantir_technologies
 import dayjs from 'dayjs';
 import unifylabs_logo from '../../assets/logos/unify_labs_logo_tagline_med.original.png';
 import cleveland_clinic_logo from '../../assets/logos/cleveland_clinic_logo.jpeg';
+import university_school_logo from '../../assets/logos/universityschool_logo.jpeg';
 
 const ExperienceTab: React.FC = () => {
   const [expandedCard, setExpandedCard] = useState<string | null>(null);
@@ -75,6 +76,54 @@ const ExperienceTab: React.FC = () => {
       endDate: dayjs('2018-08-01'),
       summary: 'Worked to develop machine learning-based methods for data cleaning and curation for Cleveland Clinic neurological data exports.',
       description: 'Worked to develop machine learning-based methods for more automatic data cleaning for datasets of medical images for neurological patients. This data was to be used for training predictive models for neurological conditions based on MRI images, and needed to be curated automatically for supervised learning. In addition, worked to better understand state-of-the-art imaging machine-learning models for future use by the research group. '
+    },
+    {
+      id: '6',
+      company: 'University School',
+      position: 'Summer Elective Teacher',
+      logo: university_school_logo,
+      startDate: dayjs('2017-06-01'),
+      endDate: dayjs('2017-07-15'),
+      summary: 'Taught two sections of an elective physics class for five weeks at an academic enrichment summer camp for gifted black middle school boys in the Greater Cleveland Area.',
+      description: 'The REACH program is a five-week rigorous summer enrichment program for academically talented African-American middle school young men in the Cleveland area. I went through it myself, and benefitted from it massively as it both accelerated my learning and introduced me to the world of college-prepatory schools. I had the opportunity in the summer of 2017 to give back by serving as a teacher of an elective course for the program. I decided to teach physics, my favorite subject from high school. I taught the boys the basic concepts of physics including kinematics, Newtonian physics, and electromagnetism. I graded their assignments, including their final project at the end of the program.',
+      externalFeatures: [
+        {
+          url: 'https://www.us.edu/academics/the-reach-program/overview',
+          title: 'REACH Program Overview'
+        }
+      ]
+    },
+    {
+      id: '7',
+      company: 'Cleveland Clinic',
+      position: 'Summer Research Intern',
+      logo: cleveland_clinic_logo,
+      startDate: dayjs('2016-06-01'),
+      endDate: dayjs('2016-08-01'),
+      summary: 'Conducted data analysis for a neuroscience lab at the Cleveland Clinic\'s Mellen Center, correlating cortical thickness with verbal fluency in a diseased cohort. Wrote and published a research paper on the findings.',
+      description: 'As part of the Dr. Stephen E. Jones MD lab, I conducted statistical analysis over a cohort of neurological patient data. The data included thicknesses of several lobes of the cerebral cortex and scores from verbal fluency tests taken by the patients, and we looked for statistically significant correlations between the two. I did this work as part of my year-long Science Research Program at Choate Rosemary Hall. Two years later, when I interned once again with Dr. Jones, we finalized the paper and submitted it for publishing.',
+      externalFeatures: [
+        {
+          url: 'https://www.sciencedirect.com/science/article/abs/pii/S0028393219300764?via%3Dihub',
+          title: 'Relationship between cortical thickness and fluency in the memory disorders clinic population'
+        }
+      ]
+    },
+    {
+      id: '8',
+      company: 'University School',
+      position: 'Summer Academic Camp Counselor',
+      logo: university_school_logo,
+      startDate: dayjs('2015-06-01'),
+      endDate: dayjs('2015-07-15'),
+      summary: 'Worked as a camp counselor assisting teachers during classes and running administrative tasks.',
+      description: 'I worked with the REACH program (see the link for an explanation of the program) as a counselor, assisting teachers with administrative tasks and supervising the students during classes and activities. ',
+      externalFeatures: [
+        {
+          url: 'https://www.us.edu/academics/the-reach-program/overview',
+          title: 'REACH Program Overview'
+        }
+      ]
     }
   ];
 
@@ -92,7 +141,6 @@ const ExperienceTab: React.FC = () => {
       </div>
 
       <div className="relative">
-        {/* Timeline line */}
         <div className="absolute left-8 top-0 bottom-0 w-0.5 bg-gradient-to-b from-[#6a92de] to-[rgb(167,194,230)]"></div>
         
         <div className="space-y-8">
